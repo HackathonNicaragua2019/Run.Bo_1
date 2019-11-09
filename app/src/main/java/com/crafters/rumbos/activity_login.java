c2f5fefadcdfd97b4e7e54efc6f0bc0fce00fcd2
SaturnoMichael <michaelespinozabetanco@gmail.com>
2019-11-09 15:17:14 -0600

    problemas login

--- app/src/main/java/com/crafters/rumbos/activity_login.java
+++ app/src/main/java/com/crafters/rumbos/activity_login.java
@@ -1,11 +1,18 @@
 package com.crafters.rumbos;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.aditya.bustrack.R;
+
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 
@@ -14,8 +21,11 @@ public class activity_login extends AppCompatActivity {
 
     private CoordinatorLayout coordinatorLayout;
     private AnimationDrawable animationDrawable;
-
-
-
+    TextInputLayout email;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        email=(TextInputLayout) findViewById(R.id.text_input_layout_email);
     }
+}
 
