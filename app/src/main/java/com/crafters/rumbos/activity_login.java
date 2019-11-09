e89939c6098af3572e1ce04f049425c0e8c55e94
Roberto-Prog <mantenimientodepcroberto2015@gmail.com>
2019-11-09 14:09:22 -0600

    Todo

--- app/src/main/java/com/crafters/rumbos/activity_login.java
+++ app/src/main/java/com/crafters/rumbos/activity_login.java
@@ -1,9 +1,20 @@
 package com.crafters.rumbos;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import butterknife.BindView;
 
 
 public class activity_login extends AppCompatActivity {
+
+    private CoordinatorLayout coordinatorLayout;
+    private AnimationDrawable animationDrawable;
+
 
 
     }
