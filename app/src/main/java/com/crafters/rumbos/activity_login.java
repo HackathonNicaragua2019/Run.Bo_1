a6524abbe67fbb68035ed1fe641f4eea5dc0c9b9
Roberto-Prog <mantenimientodepcroberto2015@gmail.com>
2019-11-09 13:34:54 -0600

    Correcion errores librerias

--- app/src/main/java/com/crafters/rumbos/activity_login.java
+++ app/src/main/java/com/crafters/rumbos/activity_login.java
@@ -1,15 +1,13 @@
 package com.crafters.rumbos;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class activity_login extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+
+
 
     }
-}
+
